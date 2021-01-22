094e12e88ddec697991dc2c63411414be700bb1e
mokhs00 <mokhs00@naver.com>
2021-01-23 01:01:31 +0900

    fix : compile error

--- record.php
+++ record.php
@@ -1,6 +1,6 @@
 <?php include 'db.php';
     $resultId= $_POST["resultId"];
-    $query('INSERT INTO record (id) VALUES(?)');
+    $query = 'INSERT INTO record (result_id) VALUES(?)';
     $stmt = $db->prepare($query);
     $stmt->execute(array($resultId));
 ?>
