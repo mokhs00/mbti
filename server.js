d0bb40fee2d79efb7d9b8178cf13a977f48e91a6
mokhs00 <mokhs00@naver.com>
2021-10-30 22:47:25 +0900

    chore : run server log update

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, 'static')))
+const port = process.env.PORT || 80;
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve('index.html'))
-})
+app.use("/static", express.static(path.resolve(__dirname, "static")));
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve("index.html"));
+});
 
-app.listen(process.env.PORT || 80, () => console.log('Server running...'))
-
+app.listen(port, () => console.log(`Server running... port : ${port}`));
